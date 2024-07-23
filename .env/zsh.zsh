dd515511ef38516cf1e368eb665bcd9a1b0d5878
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2024-07-23 07:12:56 -0600

    Fix layout & add build

--- .env/zsh.zsh
+++ .env/zsh.zsh
@@ -4,4 +4,13 @@ load ~/.zsh/environments/basic.zsh
 
 alias emacs="emacs -nw --load $PWD/.env/emacs.el"
 
+function build() {
+  test -d target || mkdir target
+  if context Resume.tex CoverLetter.tex; then
+    mv Resume.pdf "target/Jakub Stastny Resume.pdf"
+    mv CoverLetter.pdf "target/Jakub Stastny Cover Letter.pdf"
+    rm *.log *.tuc
+  fi
+}
+
 report-custom-functions
